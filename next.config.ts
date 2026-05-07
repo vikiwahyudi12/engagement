b293059f492b01427eb6a75f65267955d107248d
Viki Wahyudi <viki.wahyudi@aro.co.id>
2026-05-07 15:10:16 +0700

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  
+
+  images: {
+    unoptimized: true,
+  },
+
+
+  basePath: '/engagement',
+  assetPrefix: '/engagement',
 };
 
 export default nextConfig;
